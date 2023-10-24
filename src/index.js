61fccc375ce5d7c191ace1b0590a3df32ea5a781
Costantin Bignoli <cosbgn@gmail.com>
2023-10-24 22:17:03 +0200

    removed oas-normalize

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Oas from 'oas'
-import OASNormalize from 'oas-normalize';
-
+// import OASNormalize from 'oas-normalize';
+import {parse} from 'yaml'
 export default {
 	async fetch(request, env, ctx) {
 		const url = "https://raw.githubusercontent.com/stefanwille/openapi-generator-typescript-example/master/json-placeholder-api.yaml"
-		const normalized_yaml = await new OASNormalize(url).validate({ convertToLatest:true })
+		
+		const yaml_file = await fetch(url).then(r => r.text())
+		const normalized_yaml = parse(yaml_file)
+
+		// const normalized_yaml = await new OASNormalize(url).validate({ convertToLatest:true })
 		const oas_specs = new Oas(normalized_yaml)
 		const info = oas_specs.info
 		return new Response(info)
