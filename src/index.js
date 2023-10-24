01bed666d08fa93d9126fc5652ea2b19b5361f6f
Costantin Bignoli <cosbgn@gmail.com>
2023-10-24 21:51:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Oas from 'oas'
+import OASNormalize from 'oas-normalize';
+
+export default {
+	async fetch(request, env, ctx) {
+		const url = "https://raw.githubusercontent.com/stefanwille/openapi-generator-typescript-example/master/json-placeholder-api.yaml"
+		const normalized_yaml = await new OASNormalize(url).validate({ convertToLatest:true })
+		const oas_specs = new Oas(normalized_yaml)
+		const info = 'hey' // oas_specs.info
+		return new Response(info)
+	},
+}
