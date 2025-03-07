3b33844736e7dd887452b287447675c6f631648f
Kanad Gupta <git@kanad.dev>
2025-03-07 11:43:03 -0600

    fix: attempt to get oas-normalize working
    spoiler: it doesn't due to this error...
    
    ```
    ✘ [ERROR] service core:user:my-worker: Uncaught Error: Dynamic require of "./impl/format" is not supported
    
        at null.<anonymous> (index.js:14:9)
        at null.<anonymous> (index.js:28681:25)
        at null.<anonymous> (index.js:28671:17)
        at null.<anonymous> (index.js:28677:7) in node_modules/jsonc-parser/lib/umd/main.js
        at null.<anonymous> (index.js:20:50) in __require2
        at null.<anonymous> (index.js:32107:17) in node_modules/@readme/postman-to-openapi/src/index.js
        at null.<anonymous> (index.js:20:50) in __require2
        at null.<anonymous> (index.js:60912:41)
    ```

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import Oas from 'oas'
-// import OASNormalize from 'oas-normalize';
-import {parse} from 'yaml'
+import OASNormalize from 'oas-normalize';
 export default {
 	async fetch(request, env, ctx) {
 		const url = "https://raw.githubusercontent.com/stefanwille/openapi-generator-typescript-example/master/json-placeholder-api.yaml"
-		
-		const yaml_file = await fetch(url).then(r => r.text())
-		const normalized_yaml = parse(yaml_file)
-
-		// const normalized_yaml = await new OASNormalize(url).validate({ convertToLatest:true })
+		const normalized_yaml = await new OASNormalize(url).validate({ convertToLatest:true })
 		const oas_specs = new Oas(normalized_yaml)
 		const info = oas_specs.api.info
 		return Response.json(info)
